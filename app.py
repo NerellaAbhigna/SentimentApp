b263f0c3f143846fbe3e2d025546d10c6893be48
taraka1328 <tarakalakshmiprasanna286@gmail.com>
2026-02-13 22:36:39 +0530

    funtion

--- app.py
+++ app.py
@@ -53,5 +53,6 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
     #app.run(debug=True)
