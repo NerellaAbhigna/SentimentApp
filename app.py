15899910d5b6c3057cbb47ddecea21cf8bf44e65
taraka1328 <141255334+taraka1328@users.noreply.github.com>
2026-02-13 23:18:14 +0530

    fall

--- app.py
+++ app.py
@@ -53,6 +53,6 @@ def index():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ["PORT"])
     app.run(host="0.0.0.0", port=port)
     #app.run(debug=True)
